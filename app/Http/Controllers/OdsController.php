c8ca280938cd02d0fb4e634611d819339e035441
Rodrigo <para_332@hotmail.com>
2023-05-27 22:32:40 -0300

    Atualisamos projeto aprovado ou reprovado

--- app/Http/Controllers/OdsController.php
+++ app/Http/Controllers/OdsController.php
@@ -17,8 +17,8 @@ public function show($id)
 {
     $ods = DB::select('SELECT * FROM odss o where o.idODS=?',[$id]);
     $projetos = DB::select('SELECT p.* FROM projet_odss po join projetos p on po.idProjeto = p.idProjeto
-    WHERE po.idODS = ?',[$id]);
-    $total=DB::select('SELECT count(p.idProjeto) as total from projetos p')[0];
+    WHERE po.idODS = ? and p.aprovado = 1',[$id]);
+    $total=DB::select('SELECT count(p.idProjeto) as total from projetos p where p.aprovado = 1')[0];
     return view('ods')->with('ods',  $ods[0])
     ->with('projetos',  $projetos)
     ->with('total',$total)
